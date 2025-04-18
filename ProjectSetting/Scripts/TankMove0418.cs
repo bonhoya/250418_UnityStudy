53fe15c720f2a160a7e5400ac1448cfd9d0e42a7
bonhoya <imhoya0407@naver.com>
2025-04-18 16:39:38 +0900

    [feat] 터렛의 회전 구현
    터렛의 회전을 구현하였다.

--- ProjectSetting/Scripts/TankMove0418.cs
+++ ProjectSetting/Scripts/TankMove0418.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TreeEditor;
 using UnityEngine;
 
 public class TankMove0418 : MonoBehaviour
 {
+    [Header("탱크 움직임 제한")]
     [Range(1, 50)]
     [SerializeField] float tankSpeed;
     [Range(10, 100)]
     [SerializeField] float tankRotateSpeed;
 
+    [Header("터렛 움직임 제한")]
+    [SerializeField] GameObject turretPrefab;
+    private float turretRotateSpeed = 50;
+
     private void Update()
     {
         Mover();
+        RotateTurret();
     }
 
     private void Mover()
@@ -41,4 +48,17 @@ public class TankMove0418 : MonoBehaviour
             transform.rotation = Quaternion.Lerp(transform.rotation, quaternionVector3, tankRotateSpeed * Time.deltaTime);
         }
     }
+
+    private void RotateTurret()
+    {
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            turretPrefab.transform.Rotate(Vector3.up, turretRotateSpeed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            turretPrefab.transform.Rotate(Vector3.up, -turretRotateSpeed * Time.deltaTime);
+        }
+    }
+
 }
